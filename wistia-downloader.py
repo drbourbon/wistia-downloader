071bc0f9c82c65ad3587c2185aba6db6b893d122
fabio.barbon <fabio.barbon@gmail.com>
2020-04-07 05:03:27 +0200

    config file in json format

--- wistia-downloader.py
+++ wistia-downloader.py
@@ -2,27 +2,16 @@
 import os
 import time
 import sys
+import json
 
 video_index = 1
-def logo():
-	sprint("""\033[1;33;40m
-		 _  _  _ _           _          _____                    
-		| || || (_)     _   (_)        (____ \                   
-		| || || |_  ___| |_  _  ____    _   \ \ ___  _ _ _ ____  
-		| ||_|| | |/___)  _)| |/ _  |  | |   | / _ \| | | |  _ \ 
-		| |___| | |___ | |__| ( ( | |  | |__/ / |_| | | | | | | |
-		 \______|_(___/ \___)_|\_||_|  |_____/ \___/ \____|_| |_|
-	\033[0;37;40m""", 0.1)
+filename = 'videos.json'
+
+def sprint(string, tm=1):
+	print(string)
 
 
-def sprint(string, tm=10):
-    for c in string + '\n' :
-        sys.stdout.write(c)
-        sys.stdout.flush()
-        time.sleep(tm / 100)
-
-
-def html_page_handle_and_download(file_name):
+def html_page_handle_and_download(file_name, name):
 	print("\033[1;32;40m")
 	global video_index
 	with open(file_name, "r") as file:
@@ -41,7 +30,7 @@ def html_page_handle_and_download(file_name):
 			new_video_url = video_url.split("url")[1].split(',')[0].split('"')[2]
 			os.system("curl -O {}".format(new_video_url))
 			video_name = video_url.split("url")[1].split(',')[0].split('"')[2].split("/")[4]
-			video_name_and_index = "Video " + str(video_index) + ".mp4"
+			video_name_and_index = name # "Video " + str(video_index) + ".mp4"
 			video_index += 1
 			os.rename(video_name, video_name_and_index)
 		os.system("rm -rf {}".format(file_name))
@@ -59,10 +48,10 @@ def download_folder():
 		print("\033[0;37;40m")
 		folder_path += "/wistia-downloader/"
 		os.system("mkdir {} 2>/dev/null".format(folder_path))
-		os.system("cp video_ID.txt {} 2>/dev/null".format(folder_path))
+		os.system("cp {} {} 2>/dev/null".format(filename, folder_path))
 		os.chdir("{}".format(folder_path))
-		sprint("\033[1;33;40mOpening the download folder...\033[0;37;40m", 2)
-		os.system("open {}".format(folder_path))
+#		sprint("\033[1;33;40mOpening the download folder...\033[0;37;40m", 2)
+#		os.system("open {}".format(folder_path))
 	except FileNotFoundError:
 		print("\033[1;31;40mThe PATH does not exist !! try again\033[0;37;40m")
 		sprint(".....", 100)
@@ -71,20 +60,26 @@ def download_folder():
 
 def main():
 	os.system("clear")
-	logo()
 	download_folder()
 	url = "http://fast.wistia.net/embed/iframe/"
 	i = 0
-	with open("video_ID.txt", "r+") as video_id:
-		if os.stat("video_ID.txt").st_size == 0:
+	with open(filename, "r+") as video_id:
+		if os.stat(filename).st_size == 0:
 			print("")
-			sprint("\033[1;31;40mThe file video_ID.txt is empty !!\033[0;37;40m", 0.2)
+			sprint("\033[1;31;40mThe file is empty !!\033[0;37;40m", 0.2)
 			sprint(".....", 100)
 			main()
-		for id in video_id:
+		ids = json.load(video_id)
+		for record in ids:
+			if 'disabled' in record:
+				continue 
+			id = record['embedUrl'].rsplit('/', 1)[-1]
+			name = record['name'].split('IEC',1)[1]
+			print('id:' + id + ', name: ' + name)
+
 			os.system("curl -O {}".format(url + id))
-			print("Video ID " + str(i + 1) + "--------------------------------------------------------------------------")
-			html_page_handle_and_download(id.rstrip())
+			print("Video ID " + str(i + 1) + ' ' + name + " -------------------------------------------------")
+			html_page_handle_and_download(id.rstrip(), name)
 			print("-----------------------------------------------------------------------------------")
 			i+=1 
 
